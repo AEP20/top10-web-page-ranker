2f1efe5d6e5959989a23ec25954c362d389c18ea
Ahmet Emre Parmaksiz <ahmetemreparmaksiz@gmail.com>
2023-12-19 21:53:39 +0300

    Initial Commit

---
+++ BAU_CMP_Web_Server_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BAU_CMP_Web_Server_Project
+//
+//  Created by Ahmet Emre Parmaksız on 19.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
